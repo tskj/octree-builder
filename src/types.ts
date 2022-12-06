c835a1d7225c8943d8107010a2370d6074ed60cd
tarjei <tarjei@imerso.com>
2022-12-06 10:24:07 +0100

    octree defintion

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,19 @@ export type Point = {
     y: number;
     z: number;
 }
+
+type Octants = {
+    posX_posY_posZ: Octree;
+    posX_posY_negZ: Octree;
+    posX_negY_posZ: Octree;
+    posX_negY_negZ: Octree;
+    negX_posY_posZ: Octree;
+    negX_posY_negZ: Octree;
+    negX_negY_posZ: Octree;
+    negX_negY_negZ: Octree;
+}
+
+export type Octree =
+    | ['node', Octants]
+    | ['leaf', Point]
+    | ['empty']
